823243bce75edbbef3c4609878356190a7d6cdfa
Alimzhan <alimzhansabitzhanov@gmail.com>
2024-11-25 15:37:03 +0500

    exam5

---
+++ js/script.js
@@ -0,0 +1,35 @@
+/* //2nd exercise
+let san = +prompt("Bagasyn engiziniz: ")
+let res;
+
+if(san>=0 && san<5000){
+    alert("Jenildik jok")
+}else if(san>=5000 && san<=10000){
+    res = san * 0.1
+    alert("jenildik 10%")
+}else if(san>=10000 && san<=20000){
+    res = san * 0.2
+    alert("jenildik 20%")
+}else if(san>=20000){
+    res = san * 0.3
+    alert("jenildik 30%")
+}else{
+    alert("Duris bagasyn engiz!")
+} */
+
+/* //3rd exercise
+let num1 = +prompt("Sandy engiz: ")
+
+if(num1>=50 && num1<=50){
+    alert("Sen taptyn")
+}else if(num1>=0 && num1<=25){
+    alert("Jay jogary")
+}else if(num1>=25 && num1<=50){
+    alert("Sal jogary")
+}else if(num1>=50 && num1<=75){
+    alert("Sal tomen")
+}else if(num1>=75 && num1<=100){
+    alert("Jay tomen")
+}else{
+    alert("1den 100ge deiyn san tanda! ")
+} */
